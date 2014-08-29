ce372f31c398c9abe22c9d3220334b5bdc9b9ce4
Fahim Chowdhury <fahim@gmx.com>
2014-08-29 15:43:05 +0100

    added download feature

--- App/js/app.js
+++ App/js/app.js
@@ -293,12 +293,29 @@ angular.module('titleBar').controller('titleBarController',function($scope,$root
 	{
 		init:function(){
 			$scope.subtitle = "Example App";
+			$scope.onSave = this.onSave.bind(this);
 		},
 		onCloseClicked:function(){
 			if(chrome.app.window)chrome.app.window.current().close();
 		},
 		onHomeClicked:function(){
 			$location.path('/');
+		},
+		onSave:function(){
+			var elem = document.getElementsByClassName('wire-canvas');
+			if(elem[0])
+			{
+				html2canvas(elem[0], {
+			onrendered: function(canvas) {
+				// document.getElementById('viewer').appendChild(canvas);
+				 // window.open(canvas.toDataURL());
+				 var link = document.getElementById('downloadButton');
+				 link.classList.remove('hide');
+				 	link.href = canvas.toDataURL();
+				 	link.download = "wireframe.png";
+				}
+			});
+			}
 		}
 	};
 
